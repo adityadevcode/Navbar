12448d13c78bbdf6150d7048204cafa017345bfd
addycoderepo <adityagitcode@gmail.com>
2022-05-26 22:39:43 -0400

    Responsive Navigation Bar

--- navbar.js
+++ navbar.js
@@ -1,2 +1,15 @@
-let name = "test";
-console.log(name);
+/* when we click on the hamburger we want to see menu item*/
+const hamburger = document.getElementById('hamburger')
+hamburger.addEventListener('click', function(e) {
+const ul = document.querySelector('nav > ul');
+ul.classList.toggle('menu-slide');
+hamburger.classList.toggle('cross');
+});
+
+
+
+
+
+
+
+
